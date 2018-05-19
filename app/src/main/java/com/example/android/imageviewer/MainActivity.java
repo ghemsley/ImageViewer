88df866b7dd5c83e46c11d983d5fe3aa9d2f0775
ghemsley <ghemsley@protonmail.ch>
2018-05-19 10:31:30 -0500

    Added gallery mode
    - Directories of images can now be opened with all images displayed in a scrolling layout

--- app/src/main/java/com/example/android/imageviewer/MainActivity.java
+++ app/src/main/java/com/example/android/imageviewer/MainActivity.java
@@ -10,7 +10,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.android.example.imageviewer.MESSAGE";
@@ -19,14 +18,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.mainToolbar);
         setSupportActionBar(toolbar);
 
-     final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final FloatingActionButton fabImage = (FloatingActionButton) findViewById(R.id.fabImage);
+        fabImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                performFileSearch();
+                chooseImage();
+            }
+        });
+
+        final FloatingActionButton fabDirectory = (FloatingActionButton) findViewById(R.id.fabDirectory);
+        fabDirectory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseDirectory();
             }
         });
     }
@@ -55,12 +62,13 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private static final int READ_REQUEST_CODE = 42;
+    private static final int READ_REQUEST_CODE_IMAGE = 42;
+    private static final int READ_REQUEST_CODE_DIRECTORY = 24;
 
     /**
      * Fires an intent to spin up the "file chooser" UI and select an image.
      */
-    public void performFileSearch() {
+    public void chooseImage() {
 
         // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file
         // browser.
@@ -76,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
         // it would be "*/*".
         intent.setType("image/*");
 
-        startActivityForResult(intent, READ_REQUEST_CODE);
+        startActivityForResult(intent, READ_REQUEST_CODE_IMAGE);
 
 
 //        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
@@ -84,21 +92,36 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void chooseDirectory() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+        startActivityForResult(intent, READ_REQUEST_CODE_DIRECTORY);
+    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
         // The ACTION_OPEN_DOCUMENT intent was sent with the request code
         // READ_REQUEST_CODE. If the request code seen here doesn't match, it's the
         // response to some other intent, and the code below shouldn't run at all.
-        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+        if (requestCode == READ_REQUEST_CODE_IMAGE && resultCode == Activity.RESULT_OK) {
             // The document selected by the user won't be returned in the intent.
             // Instead, a URI to that document will be contained in the return intent
             // provided to this method as a parameter.
             // Pull that URI using resultData.getData().
-            if (resultData != null) {   //TODO: fix toast not appearing on time
-                Toast loadingToast = Toast.makeText(getBaseContext(), "Loading image...", Toast.LENGTH_SHORT);
-                loadingToast.show();
+            if (resultData != null) {
                 Uri uri = resultData.getData();
                 Intent intent = new Intent(this, FullscreenActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, uri.toString());
+                startActivity(intent);
+            }
+        }
+        if (requestCode == READ_REQUEST_CODE_DIRECTORY && resultCode == Activity.RESULT_OK) {
+            // The document selected by the user won't be returned in the intent.
+            // Instead, a URI to that document will be contained in the return intent
+            // provided to this method as a parameter.
+            // Pull that URI using resultData.getData().
+            if (resultData != null) {
+                Uri uri = resultData.getData();
+                Intent intent = new Intent(this, GalleryActivity.class);
                 intent.putExtra(EXTRA_MESSAGE, uri.toString());
                 startActivity(intent);
             }
